009bc6356366777f46698501c68d91a803169fbe
Michal Szpakowski <michal.szpakowski@netguru.co>
2019-08-16 02:03:58 +0200

    Define routes

--- lib/football_scores_web/router.ex
+++ lib/football_scores_web/router.ex
@@ -1,11 +1,16 @@
 defmodule FootballScoresWeb.Router do
   use FootballScoresWeb, :router
 
-  pipeline :api do
-    plug :accepts, ["json"]
+  alias FootballScoresWeb.{DivisionSeasonController, GameController}
+
+  pipeline :v1 do
+    plug :accepts, ["json", "x-protobuf"]
   end
 
-  scope "/api", FootballScoresWeb do
-    pipe_through :api
+  scope "/v1" do
+    pipe_through :v1
+
+    resources "/division_seasons", DivisionSeasonController, only: [:index] do
+      resources "/games", GameController, only: [:index]
   end
 end
